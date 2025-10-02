4900f446fe2fc1e89cda091a728fca5e1ecae7c6
sundara26071978 <sunjsg@gmail.com>
2025-10-02 10:49:55 +0530

    multiply function in file.py

--- Folder/file.py
+++ Folder/file.py
@@ -12,3 +12,7 @@ def divide(a: int, b: int) -> float:
     if b == 0:
         raise ValueError("Cannot divide by zero.")
     return a / b
+
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers."""
+    return a * b
