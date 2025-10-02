e03db766793b7038ab7d88540496a472b661a3aa
sundara26071978 <sunjsg@gmail.com>
2025-10-02 10:55:28 +0530

    add function

--- Folder/file.py
+++ Folder/file.py
@@ -16,3 +16,7 @@ def divide(a: int, b: int) -> float:
 def multiply(a: int, b: int) -> int:
     """Multiply two numbers."""
     return a * b
+
+def add(a: int, b: int) -> int:
+    """Add two numbers."""
+    return a + b
